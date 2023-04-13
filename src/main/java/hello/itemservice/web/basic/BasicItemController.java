18882086c735da612d278319642cb77dd6eb5301
yanapang <yanahwang@gmail.com>
2023-04-13 23:25:18 +0900

    [스프링 MVC 웹페이지 만들기] 상품 등록 Form.
    post로 form내용 보낼때 동일 url일 경우 th:action값을 비워도 제대로 동작. 현재 url에 데이터를 전송한다.
    (참고. addForm.html)
    
    취소시 상품 목록으로 이동하게 수정함.

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -40,11 +41,22 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "basic/addForm";
+    }
+
+
     /**
      * Test용 데이터 추가
      */
     @PostConstruct
-    public void init(){
+    public void init() {
         itemRepository.save(new Item("itemA", 1000, 3));
         itemRepository.save(new Item("itemB", 300, 8));
     }
