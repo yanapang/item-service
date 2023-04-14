e84bebf237443141adf24389637fc37b31e6945b
yanapang <yanahwang@gmail.com>
2023-04-14 14:25:50 +0900

    [스프링 MVC 웹페이지 만들기] - RedirectAttribute. redirect시 Model처럼 Attribute를 담아 전달 가능.

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -99,10 +100,18 @@ public class BasicItemController {
      * 새로고침시 같은 값 계속 submit 하는 문제 해결.
      * 상품 상세 페이지로 redirect 됨.
      */
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV5(Item item) {
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId(); //목록으로이동.
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}"; //목록으로이동.
     }
 
     //상품 수정.
